c2e0b661558e093ee306a835cd16fe626ae9df99
Jonathan Portillo <54336208+jonathan-portillo@users.noreply.github.com>
2021-04-22 00:12:29 -0400

    clean up and adjusted navBar

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,12 +3,9 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      <h1>Hi, my name is Jonathan Portillo</h1>
-      <h1>
-        Currently my profile is under constructions. Can't wait to get this up
-        and running!
-      </h1>
-      ;
+      <div className="nameContainer">
+        <h1 className="myName">Hi, my name is Jonathan Portillo</h1>
+      </div>
     </>
   );
 };
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,21 +4,23 @@ import Breadcrumbs from "@material-ui/core/Breadcrumbs";
 
 const Navbar = () => {
   return (
-    <div className="navLinks">
-      <Breadcrumbs aria-label="breadcrumb">
-        <NavLink color="inherit" to="/">
-          Home
-        </NavLink>
-        <NavLink color="inherit" to="/projects">
-          My Projects
-        </NavLink>
-        <NavLink color="inherit" to="/skills">
-          My Skills
-        </NavLink>
-        <NavLink color="inherit" to="/about">
-          About Me
-        </NavLink>
-      </Breadcrumbs>
+    <div className="navBar">
+      <div className="navLinks">
+        <Breadcrumbs aria-label="breadcrumb">
+          <NavLink color="inherit" to="/">
+            Home
+          </NavLink>
+          <NavLink color="inherit" to="/projects">
+            My Projects
+          </NavLink>
+          <NavLink color="inherit" to="/skills">
+            My Skills
+          </NavLink>
+          <NavLink color="inherit" to="/about">
+            About Me
+          </NavLink>
+        </Breadcrumbs>
+      </div>
     </div>
   );
 };
